720a7a2d5af711e99f5713a1300ab90738df151a
Dillon Teakell <teakelld@icloud.com>
2023-11-06 21:26:56 -0500

    Initial Commit

---
+++ DeveloperProfile-UI/DeveloperProfile_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperProfile_UIApp.swift
+//  DeveloperProfile-UI
+//
+//  Created by Dillon Teakell on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperProfile_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
